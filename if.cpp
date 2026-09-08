0ed27148d4f38ecf5867dddd9b8b89d453020fd4
Mitsuhiri <algifarifahmi1@gmail.com>
2026-09-08 18:02:50 +0700

    First jomok

---
+++ if.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int nilai;
+
+    cout<<"Masukan Nilai"<<endl;
+    cin>>nilai;
+    
+    if (nilai >= 80) {
+        cout<<'A'<<endl;
+    } else if(68 <= nilai < 80) {
+        cout<<'B'<<endl;
+    } else if(56 <= nilai < 68) {
+        cout<<'C'<<endl;
+    } else if(56 <= nilai < 68) {
+        cout<<'D'<<endl;
+    } else {
+        cout<<'E'<<endl;
+    }
+    if (nilai >=65){
+        cout<<"LULUS"<<endl;
+    } else {
+        cout<<"TIDAK LULUS"<<endl;
+    }
+    return 0;
+}
